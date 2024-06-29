9c0297189e095b9942df9ddc4402389efb4967a6
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-29 09:50:39 +0300

    Initial Commit

---
+++ SwiftUICustomTimePickerLikeAppleTimerApp/SwiftUICustomTimePickerLikeAppleTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTimePickerLikeAppleTimerAppApp.swift
+//  SwiftUICustomTimePickerLikeAppleTimerApp
+//
+//  Created by Ramill Ibragimov on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTimePickerLikeAppleTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
